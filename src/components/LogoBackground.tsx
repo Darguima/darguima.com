fdf536b3a1ef42313a8f979885184b3a3b4b4811
Darguima <dsgdevbraga@gmail.com>
2025-05-01 23:36:11 +0200

    New AI GeneratedLogo

--- src/components/LogoBackground.tsx
+++ src/components/LogoBackground.tsx
@@ -3,8 +3,8 @@ export default function LogoBackground() {
     <div
       className="
           fixed
-          bottom-[-50px] right-[-350px] w-[500] h-[500]
-          md:bottom-[-250px] md:right-[-250px] md:w-[750px] md:h-[750px]
+          bottom-[-50px] right-[-300px] w-[500] h-[500]
+          md:bottom-[-200px] md:right-[-275px] md:w-[750px] md:h-[750px]
           opacity-5 z-[-10] pointer-events-none
           bg-[url('/logo.svg')] bg-no-repeat bg-right-bottom bg-contain grayscale
         "
